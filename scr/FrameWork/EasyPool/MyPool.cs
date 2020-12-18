adf7fff532a112b12e81c7a18f8d725509421b11
LoveAuggie <381724359@qq.com>
2020-12-18 10:01:58 +0800

    基础逻辑完成

--- scr/FrameWork/EasyPool/MyPool.cs
+++ scr/FrameWork/EasyPool/MyPool.cs
@@ -4,68 +4,150 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace EasyPool
 {
     public class MyPool<T> where T: class
     {
-        public ConcurrentQueue<T> list = new ConcurrentQueue<T>();
+        /// <summary>
+        /// 当前列表
+        /// </summary>
+        private List<SingelObject<T>> list = new List<SingelObject<T>>();
 
-        public ConcurrentDictionary<int, T> used = new ConcurrentDictionary<int, T>();
+        /// <summary>
+        /// 使用中的数据列表
+        /// </summary>
+        private List<SingelObject<T>> used = new List<SingelObject<T>>();
 
         private PoolObject<T> _Pobject = null;
 
-        public MyPool(PoolObject<T> pObjcet)
+        private int _TimeOut;
+
+        private int _MaxCount;
+
+        private Timer OutCheckTimer;
+
+        public int CurrentCount
+        {
+            get
+            {
+                return list.Count + used.Count;
+            }
+        }
+
+        public MyPool(PoolObject<T> pObjcet, int MaxCount = 0, int TimeOut = 0)
         {
             _Pobject = pObjcet;
+            _MaxCount = MaxCount;
+            _TimeOut = TimeOut;
+
+            OutCheckTimer = new Timer();
+            OutCheckTimer.Elapsed += OutCheckTimer_Elapsed;
+            OutCheckTimer.Interval = 1000;
+            OutCheckTimer.Enabled = true;
+        }
+
+        private void OutCheckTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                OutCheckTimer.Enabled = false;
+
+                for (int i = list.Count - 1; i > 0; i--)
+                {
+                    var st = list[i];
+                    if (st.LastUseTime.AddSeconds(_TimeOut) < DateTime.Now)
+                    {
+                        list.RemoveAt(i);
+                        _Pobject.CloseAndDispose(st.Obj);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                OutCheckTimer.Enabled = true;
+            }
         }
 
         public void Run(Action<T> act)
         {
+            SingelObject<T> st = null;
+            st = this.Get();
             T t = default(T);
+            if (st != null)
+            {
+                t = st.Obj;
+            }
             try
             {
-                t = this.Get();
                 act.Invoke(t);
             }
-            catch (Exception)
-            {
-                if (t != default(T))
-                {
-                    _Pobject.CanUse(t);
-                    if (!_Pobject.CanUse(t))
-                    {
-                        var code = t.GetHashCode();
-                        used.TryRemove(code, out T temp);
-                        t = default(T);
-                    }
-                }
-            }
+            catch { }
             finally
             {
-                if (t != default(T))
-                {
-                    Return(t);
-                }
+                if (st != null)
+                    this.Return(st);
             }
         }
 
-        private T Get()
+        private SingelObject<T> Get()
         {
-            if (!list.TryDequeue(out T t))
+            lock (list)
             {
-                t = _Pobject.Create();
+                int sum = 0;
+                while (sum++ <= 5)
+                {
+                    SingelObject<T> st = null;
+                    if (list.Count > 0)
+                    {
+                        st = list.First();
+                    }
+                    else
+                    {
+                        var t = _Pobject.Create();
+
+                        st = new SingelObject<T>()
+                        {
+                            Obj = t
+                        };
+                    }
+                    if (st.Obj != default(T) && _Pobject.CanUse(st.Obj))
+                    {
+                        st.LastUseTime = DateTime.Now;
+                        used.Add(st);
+
+                        return st;
+                    }
+                    else
+                    {
+                        list.Remove(st);
+                    }
+                    System.Threading.Thread.Sleep(200);
+                }
+                return null;
             }
-            used.TryAdd(t.GetHashCode(), t);
-            return t;
         }
 
-        private void Return(T poolObject)
+        private void Return(SingelObject<T> poolObject)
         {
-            var code = poolObject.GetHashCode();
-            used.TryRemove(code, out T temp);
+            used.Remove(poolObject);
 
-            list.Enqueue(poolObject);
+            /* 每次都放在最前面，这样若是需要的连接数比较少，
+             * 而创建的连接数又比较多的话，会导致队列尾的若干个连接一直不会被用到
+             * 从而可以被检查线程自动释放掉
+            */
+            list.Insert(0, poolObject);
         }
+    }
+
+    internal class SingelObject<T>
+    {
+        public T Obj;
+
+        public DateTime LastUseTime;
     }
 }
--- scr/TestConsole/Program.cs
+++ scr/TestConsole/Program.cs
@@ -14,28 +14,66 @@ namespace TestConsole
         {
             MyPool<MySqlConnection> pool = new MyPool<MySqlConnection>(new Mycon());
 
-            pool.Run((con) => 
+            for (int i = 0; i < 20; i++)
             {
+                int curTask = i;
+                Task.Factory.StartNew(() => 
+                {
+                    var tasknum = curTask;
+                    int sum = 0;
+                    while (sum < 100) 
+                    {
+                        pool.Run((db) => 
+                        {
+                            using (var cmd = db.CreateCommand())
+                            {
+                                cmd.CommandText = "select * from sys_menus";
+                                using (var dr = cmd.ExecuteReader())
+                                {
+                                    Console.Write(".");
+                                }
+                            }
+                            System.Threading.Thread.Sleep(100);
+                        });
+                    }
+                });
+            }
 
-            });
+            while (true)
+            {
+                Console.ReadKey();
+            }
         }
     }
 
     class Mycon : PoolObject<MySqlConnection>
     {
+        private static int TotalCount=0;
+
+        private string ConnStr;
+        public Mycon() 
+        {
+            ConnStr = "Database=jcpt_auth;Data Source=10.1.55.44;Port=3306;User Id=auggie;Password=123456;Charset=utf8;";
+        }
+
         public override bool CanUse(MySqlConnection t)
         {
-            throw new NotImplementedException();
+            return t.State == System.Data.ConnectionState.Open;
         }
 
         public override bool CloseAndDispose(MySqlConnection t)
         {
-            throw new NotImplementedException();
+            t.Close();
+            t.Dispose();
+            return true;
         }
 
         public override MySqlConnection Create()
         {
-            throw new NotImplementedException();
+            Console.WriteLine($"CreateNum => {++TotalCount}");
+            var con = new MySqlConnection(ConnStr);
+            con.Open();
+            return con;
         }
     }
 }
